3682502ccfc083221828160340960e975d2a3139
nash300 <nadeesha300@yahoo.co.uk>
2023-05-10 00:56:24 +0200

    Change some class names in main-page, made the page responsive for computer, tablet and mobile

--- src/App.js
+++ src/App.js
@@ -5,26 +5,23 @@ import "./css/app.css";
 function App() {
   return (
     <>
-      <div className="main-wrapper">
-        <div className="wrapper-Banner">
-          <div className="sub-wrapper">
-            <div className="image-container">
-              <img src="images/logo.jpg" alt="logo picture" />
-            </div>
-            <div className="form-container">
-              <div className="button-container">
-                <a href="./Student">
-                  <button>I amm a student. Looking for a teacher</button>
-                </a>
-                <a href="/Teacher">
-                  <button> I am a Teacher. Looking for a student</button>
-                </a>
-              </div>
+      <div className="container">
+        <div className="banner-wrapper">
+          <div className="image-container">
+            <img src="images/logo.jpg" alt="logo picture" />
+          </div>
+          <div className="form-container">
+            <div className="button-container">
+              <a href="./Student">
+                <button>I am a student. Looking for a teacher</button>
+              </a>
+              <a href="/Teacher">
+                <button> I am a Teacher. Looking for a student</button>
+              </a>
             </div>
           </div>
         </div>
       </div>
-      )
     </>
   );
 }
